c7c832a900e268bbf7cf152d67aa1d24644d2fcb
ThanhDatTu3tle <71086168+ThanhDatTu3tle@users.noreply.github.com>
2022-10-28 22:44:45 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,39 @@
-import React from "react";
-
 import { Refine } from "@pankod/refine-core";
 import {
-  notificationProvider,
-  RefineSnackbarProvider,
-  CssBaseline,
-  GlobalStyles,
-  Layout,
-  ThemeProvider,
-  LightTheme,
-  ReadyPage,
-  ErrorComponent,
+    Layout,
+    ErrorComponent,
+    ReadyPage,
+    LightTheme,
+    CssBaseline,
+    ThemeProvider,
+    GlobalStyles,
+    RefineSnackbarProvider,
+    notificationProvider,
 } from "@pankod/refine-mui";
-
 import routerProvider from "@pankod/refine-react-router-v6";
 import dataProvider from "@pankod/refine-simple-rest";
 
-function App() {
-  return (
-    <ThemeProvider theme={LightTheme}>
-      <CssBaseline />
-      <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
-      <RefineSnackbarProvider>
-        <Refine
-          notificationProvider={notificationProvider}
-          Layout={Layout}
-          ReadyPage={ReadyPage}
-          catchAll={<ErrorComponent />}
-          routerProvider={routerProvider}
-          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-        />
-      </RefineSnackbarProvider>
-    </ThemeProvider>
-  );
-}
+import { PostList, PostShow, PostEdit, PostCreate  } from "pages/posts";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+const App: React.FC = () => {
+    return (
+        <ThemeProvider theme={LightTheme}>
+            <CssBaseline />
+            <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+            <RefineSnackbarProvider>
+                <Refine
+                    routerProvider={routerProvider}
+                    dataProvider={dataProvider(API_URL)}
+                    notificationProvider={notificationProvider}
+                    Layout={Layout}
+                    ReadyPage={ReadyPage}
+                    catchAll={<ErrorComponent />}
+                    resources={[{ name: "posts", list: PostList, show: PostShow, edit: PostEdit, create: PostCreate, canDelete: true }]}
+                />
+            </RefineSnackbarProvider>
+        </ThemeProvider>
+    );
+};
 
 export default App;
